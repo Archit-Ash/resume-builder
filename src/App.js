1cb5c2dd6d9f6e37347f7db6ea5e37f1181d6dcb
Archit Shukla <asharchit03@gmail.com>
2023-11-01 20:54:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,182 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import PersonalInfo from "./form";
+import jsPDF from "jspdf";
+
+const generatePdf = (selfinfo) => {
+  const doc = new jsPDF();
+  doc.addFont("calibri");
+  doc.setFont("calibri");
+
+  var pageWidth =
+    doc.internal.pageSize.width || doc.internal.pageSize.getWidth();
+
+  //biodata
+  doc.setTextColor("#576C7A");
+  doc.setFontSize(30);
+  doc.textWithLink(selfinfo.selfName.toUpperCase(), pageWidth / 2, 15, {
+    align: "center",
+    url: selfinfo.profile,
+  });
+  doc.setDrawColor("#576C7A");
+  doc.setLineWidth(0.1);
+  doc.line(5, 17, pageWidth - 5, 17);
+  doc.setLineWidth(0.5);
+  doc.line(5, 18, pageWidth - 5, 18);
+  doc.setTextColor("#000000");
+  doc.setFontSize(10);
+  doc.text(selfinfo.selfLocation, pageWidth / 2, 23, {
+    align: "center",
+  });
+  doc.text(
+    selfinfo.selfEmail + "   •   " + selfinfo.selfNumber,
+    pageWidth / 2,
+    28,
+    {
+      align: "center",
+    }
+  );
+  doc.setTextColor("#576c7a");
+  doc.setFontSize(15);
+  doc.text("Education", 10, 38);
+  doc.setLineDash([1, 1], 0);
+  doc.line(10, 40, pageWidth - 10, 40);
+  doc.setTextColor("#000000");
+  doc.setFontSize(12);
+  doc.setTextColor("#576c9f");
+  doc.text("HIGHSCHOOL", 45, 47);
+  doc.setTextColor("#000000");
+  doc.text(
+    selfinfo.school_name10 +
+      " | " +
+      selfinfo.percentage_10 +
+      " | " +
+      selfinfo.year_10,
+    50,
+    52
+  );
+  doc.setTextColor("#576c9f");
+  doc.text("INTERMEDIATE", 45, 59);
+  doc.setTextColor("#000000");
+  doc.text(
+    selfinfo.school_name12 +
+      " | " +
+      selfinfo.percentage_12 +
+      " | " +
+      selfinfo.year_12,
+    50,
+    64
+  );
+  doc.setTextColor("#576c9f");
+  doc.text("GRADUATION", 45, 71);
+  doc.setTextColor("#000000");
+  doc.text(
+    selfinfo.school_name12 +
+      " | " +
+      selfinfo.percentage_12 +
+      " | " +
+      selfinfo.year_12,
+    50,
+    76
+  );
+  doc.setFontSize(15);
+  doc.setTextColor("#576c7a");
+  doc.text("Projects", 10, 83);
+  doc.setLineDash([1, 1], 0);
+  doc.line(10, 85, pageWidth - 10, 85);
+  doc.setTextColor("#000000");
+  doc.setFontSize(12);
+  doc.setTextColor("#576c9f");
+  doc.textWithLink(selfinfo.projectName1, 45, 92, {
+    url: selfinfo.projectLink1,
+  });
+  doc.setTextColor("#000000");
+  doc.text(selfinfo.projectDetails1, 50, 97);
+  doc.text(selfinfo.tools1, 50, 102);
+  doc.setTextColor("#576c9f");
+  doc.textWithLink(selfinfo.projectName2, 45, 110, {
+    url: selfinfo.projectLink2,
+  });
+  doc.setTextColor("#000000");
+  doc.text(selfinfo.projectDetails2, 50, 115);
+  doc.text(selfinfo.tools2, 50, 120);
+  doc.setTextColor("#576c9f");
+  doc.textWithLink(selfinfo.projectName3, 45, 127, {
+    url: selfinfo.projectLink3,
+  });
+  doc.setTextColor("#000000");
+  doc.text(selfinfo.projectDetails3, 50, 132);
+  doc.text(selfinfo.tools3, 50, 137);
+
+  doc.setTextColor("#576c7a");
+  doc.setFontSize(15);
+  doc.text("Languages Known", 10, 144);
+  doc.setLineDash([1, 1], 0);
+  doc.line(10, 146, pageWidth - 10, 146);
+  doc.setTextColor("#576c9f");
+  doc.text(selfinfo.languages, 45, 153);
+
+  doc.setTextColor("#576c7a");
+  doc.setFontSize(15);
+  doc.text("Technical Skills", 10, 164);
+  doc.setLineDash([1, 1], 0);
+  doc.line(10, 166, pageWidth - 10, 166);
+  doc.setTextColor("#576c9f");
+  doc.setFontSize(12);
+  doc.text(selfinfo.technicalSkills, 45, 172);
+
+  doc.setTextColor("#576c7a");
+  doc.setFontSize(15);
+  doc.text("Certificates", 10, 180);
+  doc.setLineDash([1, 1], 0);
+  doc.setFontSize(12);
+  doc.line(10, 182, pageWidth - 10, 182);
+  doc.setTextColor("#576c9f");
+  doc.textWithLink(selfinfo.certification1, 45, 189, {
+    url: selfinfo.certificate1,
+  });
+  doc.textWithLink(selfinfo.certification2, 45, 196, {
+    url: selfinfo.certificate2,
+  });
+
+  doc.setTextColor("#576c7a");
+  doc.setFontSize(15);
+  doc.text("Profiles", 10, 202);
+  doc.setLineDash([1, 1], 0);
+
+  doc.line(10, 204, pageWidth - 10, 204);
+  doc.setTextColor("#576c9f");
+  doc.text("Linkedin", 45, 212);
+  doc.text("Github", 45, 224);
+  doc.setFontSize(12);
+  doc.setTextColor("#000000");
+  doc.textWithLink(selfinfo.linkedin, 50, 217, { url: selfinfo.linkedin });
+  doc.textWithLink(selfinfo.github, 50, 229, { url: selfinfo.github });
+
+  doc.setTextColor("#576c7a");
+  doc.setFontSize(15);
+  doc.text("Summary", 10, 237);
+  doc.setLineDash([1, 1], 0);
+
+  doc.line(10, 239, pageWidth - 10, 239);
+
+  doc.setTextColor("#000000");
+  doc.setFontSize(12);
+  doc.text(selfinfo.activities, 45, 247, { maxWidth: 150 });
+
+  doc.save("Resume.pdf");
+};
 
 function App() {
+  const handleFormSubmit = (selfinfo) => {
+    console.log("Form data received in App:", selfinfo);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>RESUME BUILDER</h1>
+
+      <PersonalInfo
+        onFormSubmit={(handleFormSubmit, generatePdf)}
+      ></PersonalInfo>
     </div>
   );
 }
---
+++ src/form.js
@@ -0,0 +1,479 @@
+import React, { useState } from "react";
+
+import "./main.css";
+
+const PersonalInfo = ({ onFormSubmit }) => {
+  const [formData, setData] = useState({
+    //biodata
+    selfName: "",
+    selfEmail: "",
+    selfNumber: "",
+    selfLocation: "",
+    profile: "",
+    //project Details
+    projectName1: "",
+    projectDetails1: "",
+    projectLink1: "",
+    tools1: "",
+    projectName2: "",
+    projectDetails2: "",
+    projectLink2: "",
+    tools2: "",
+    projectName3: "",
+    projectDetails3: "",
+    projectLink3: "",
+    tools3: "",
+    //Skills/interest/Activities
+    languages: "",
+    technicalSkills: "",
+    certification1: "",
+    certificate1: "",
+    certification2: "",
+    certificate2: "",
+    activities: "",
+
+    //Education
+    school_name10: "",
+    percentage_10: "",
+    year_10: "",
+    school_name12: "",
+    percentage_12: "",
+    year_12: "",
+    grad_college: "",
+    grad_gpa: "",
+    grad_year: "",
+    //links
+    github: "",
+    linkedin: "",
+  });
+  const handleInput = (field, value) => {
+    setData({
+      ...formData,
+      [field]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Pass the form data to the parent component
+    onFormSubmit(formData);
+  };
+
+  return (
+    <div>
+      <img src={require("./assets/programmer.png")} alt="" />
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Name:</label>
+          <input
+            type="text"
+            maxLength={20}
+            required
+            value={formData.selfName}
+            placeholder="Tony Stark"
+            onChange={(e) => handleInput("selfName", e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Email:</label>
+          <input
+            required
+            type="email"
+            placeholder="tony@stark.com"
+            value={formData.selfEmail}
+            onChange={(e) => handleInput("selfEmail", e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Phone Number:</label>
+          <input
+            required
+            type="text"
+            placeholder="9999999999"
+            maxLength={10}
+            value={formData.selfNumber}
+            onChange={(e) => handleInput("selfNumber", e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Address:</label>
+          <input
+            required
+            type="text"
+            placeholder="6th,aventor street"
+            maxLength={50}
+            value={formData.selfLocation}
+            onChange={(e) => handleInput("selfLocation", e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Portfolio link:</label>
+          <input
+            required
+            type="text"
+            placeholder="https://project.com"
+            value={formData.profile}
+            onChange={(e) => handleInput("profile", e.target.value)}
+          />
+        </div>
+        <div>
+          <img src={require("./assets/project.png")} alt="" />
+        </div>
+        <div>
+          <div>
+            <img src={require("./assets/number-1.png")} alt="" />
+          </div>
+          <label>Project Name</label>
+          <input
+            required
+            type="text"
+            placeholder="weather app"
+            value={formData.projectName1}
+            onChange={(e) => handleInput("projectName1", e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Project Details</label>
+          <input
+            required
+            type="text"
+            placeholder="App giving details of weather"
+            value={formData.projectDetails1}
+            onChange={(e) => handleInput("projectDetails1", e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Project Link</label>
+          <input
+            required
+            type="text"
+            placeholder="https//weather.com"
+            value={formData.projectLink1}
+            onChange={(e) => handleInput("projectLink1", e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Tools Used</label>
+          <input
+            required
+            type="text"
+            placeholder="HTML,CSS,JS"
+            value={formData.tools1}
+            onChange={(e) => handleInput("tools1", e.target.value)}
+          />
+        </div>
+
+        <div>
+          <div>
+            <img src={require("./assets/track.png")} alt="" />
+          </div>
+          <label>Project Name</label>
+          <input
+            required
+            type="text"
+            placeholder="weather app"
+            value={formData.projectName2}
+            onChange={(e) => handleInput("projectName2", e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Project Details</label>
+          <input
+            required
+            type="text"
+            placeholder="App giving details of weather"
+            value={formData.projectDetails2}
+            onChange={(e) => handleInput("projectDetails2", e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Project Link</label>
+          <input
+            required
+            type="text"
+            placeholder="https//weather.com"
+            value={formData.projectLink2}
+            onChange={(e) => handleInput("projectLink2", e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Tools Used</label>
+          <input
+            required
+            type="text"
+            placeholder="HTML,CSS,JS"
+            value={formData.tools2}
+            onChange={(e) => handleInput("tools2", e.target.value)}
+          />
+        </div>
+        <div>
+          <div>
+            <img src={require("./assets/number-3.png")} alt="" />
+          </div>
+          <label>Project Name</label>
+          <input
+            required
+            type="text"
+            placeholder="weather app"
+            value={formData.projectName3}
+            onChange={(e) => handleInput("projectName3", e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Project Details</label>
+          <input
+            required
+            type="text"
+            placeholder="App giving details of weather"
+            value={formData.projectDetails3}
+            onChange={(e) => handleInput("projectDetails3", e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Project Link</label>
+          <input
+            required
+            type="text"
+            placeholder="https//weather.com"
+            value={formData.projectLink3}
+            onChange={(e) => handleInput("projectLink3", e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Tools Used</label>
+          <input
+            required
+            type="text"
+            placeholder="HTML,CSS,JS"
+            value={formData.tools3}
+            onChange={(e) => handleInput("tools3", e.target.value)}
+          />
+        </div>
+        <div>
+          <img src={require("./assets/education.png")} alt="" />
+
+          <div>
+            <div>
+              <img src={require("./assets/school.png")} alt="" />
+            </div>
+            <label>School Name</label>
+            <input
+              required
+              type="text"
+              placeholder="St. Don Bosco International College"
+              value={formData.school_name10}
+              onChange={(e) => handleInput("school_name10", e.target.value)}
+            />
+          </div>
+
+          <div>
+            <label>School Name (10th)</label>
+            <input
+              required
+              type="text"
+              placeholder="Enter school name (10th)"
+              value={formData.school_name10}
+              onChange={(e) => handleInput("school_name10", e.target.value)}
+            />
+          </div>
+
+          <div>
+            <label>Percentage (10th)</label>
+            <input
+              required
+              type="text"
+              placeholder="Enter percentage (10th)"
+              value={formData.percentage_10}
+              onChange={(e) => handleInput("percentage_10", e.target.value)}
+            />
+          </div>
+
+          <div>
+            <label>Year (10th)</label>
+            <input
+              required
+              type="text"
+              placeholder="Enter year (10th)"
+              value={formData.year_10}
+              onChange={(e) => handleInput("year_10", e.target.value)}
+            />
+          </div>
+
+          <div>
+            <label>School Name (12th)</label>
+            <input
+              required
+              type="text"
+              placeholder="Enter school name (12th)"
+              value={formData.school_name12}
+              onChange={(e) => handleInput("school_name12", e.target.value)}
+            />
+          </div>
+
+          <div>
+            <label>Percentage (12th)</label>
+            <input
+              required
+              type="text"
+              placeholder="Enter percentage (12th)"
+              value={formData.percentage_12}
+              onChange={(e) => handleInput("percentage_12", e.target.value)}
+            />
+          </div>
+
+          <div>
+            <label>Year (12th)</label>
+            <input
+              required
+              type="text"
+              placeholder="Enter year (12th)"
+              value={formData.year_12}
+              onChange={(e) => handleInput("year_12", e.target.value)}
+            />
+          </div>
+          <img src={require("./assets/college.png")} alt="" />
+
+          <div>
+            <label>Grad College</label>
+            <input
+              required
+              type="text"
+              placeholder="Enter college name (grad)"
+              value={formData.grad_college}
+              onChange={(e) => handleInput("grad_college", e.target.value)}
+            />
+          </div>
+
+          <div>
+            <label>Grad GPA</label>
+            <input
+              required
+              type="text"
+              placeholder="Enter GPA (grad)"
+              value={formData.grad_gpa}
+              onChange={(e) => handleInput("grad_gpa", e.target.value)}
+            />
+          </div>
+
+          <div>
+            <label>Grad Year</label>
+            <input
+              required
+              type="text"
+              placeholder="Enter year of graduation (grad)"
+              value={formData.grad_year}
+              onChange={(e) => handleInput("grad_year", e.target.value)}
+            />
+          </div>
+          <div>
+            <img src={require("./assets/languages.png")} alt="" />
+            <label>Languages</label>
+            <input
+              required
+              type="text"
+              placeholder="Enter languages"
+              value={formData.languages}
+              onChange={(e) => handleInput("languages", e.target.value)}
+            />
+          </div>
+
+          <div>
+            <img src={require("./assets/4727496.png")} alt="" />
+
+            <label>Technical Skills</label>
+            <input
+              required
+              type="text"
+              placeholder="Enter technical skills"
+              value={formData.technicalSkills}
+              onChange={(e) => handleInput("technicalSkills", e.target.value)}
+            />
+          </div>
+
+          <div>
+            <img src={require("./assets/certificate.png")} alt="" />
+            <label>Certification 1</label>
+            <input
+              required
+              type="text"
+              placeholder="Enter certification 1"
+              value={formData.certification1}
+              onChange={(e) => handleInput("certification1", e.target.value)}
+            />
+          </div>
+
+          <div>
+            <label>Certificate link 1</label>
+            <input
+              required
+              type="text"
+              placeholder="www.github.com/project"
+              value={formData.certificate1}
+              onChange={(e) => handleInput("certificate1", e.target.value)}
+            />
+          </div>
+
+          <div>
+            <label>Certification 2</label>
+            <input
+              required
+              type="text"
+              placeholder="Enter certification 2"
+              value={formData.certification2}
+              onChange={(e) => handleInput("certification2", e.target.value)}
+            />
+          </div>
+          <div>
+            <label>Certificate link 2</label>
+            <input
+              required
+              type="text"
+              placeholder="www.github.com/project"
+              value={formData.certificate2}
+              onChange={(e) => handleInput("certificate2", e.target.value)}
+            />
+          </div>
+
+          <div>
+            <img src={require("./assets/activity.png")} alt="" />
+
+            <label>professional summary</label>
+            <input
+              required
+              type="text"
+              placeholder="Enter summary"
+              value={formData.activities}
+              maxLength={400}
+              onChange={(e) => handleInput("activities", e.target.value)}
+            />
+          </div>
+
+          <div>
+            <img src={require("./assets/6994770.png")} alt="" />
+            <label>Linkedin</label>
+            <input
+              required
+              type="text"
+              placeholder="wwew.linkedin.com/profile"
+              value={formData.linkedin}
+              onChange={(e) => handleInput("linkedin", e.target.value)}
+            />
+          </div>
+          <div>
+            <label>Github</label>
+            <input
+              required
+              type="text"
+              placeholder="wwew.github.com/profile"
+              value={formData.github}
+              onChange={(e) => handleInput("github", e.target.value)}
+            />
+          </div>
+        </div>
+        <button type="submit">SUBMIT</button>
+      </form>
+    </div>
+  );
+};
+
+export default PersonalInfo;
